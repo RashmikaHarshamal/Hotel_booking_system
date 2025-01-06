c002c5495c3bd67b6ce37c2f8a5b6fd715391bc5
Rashmika_Harshamal <rashmikaharshamal169@mail.com>
2025-01-06 17:56:51 +0530

    Add assets and Header component dependencies

--- hotel-booking-system/src/App.jsx
+++ hotel-booking-system/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Header from './Components/Header/Header'
 import Nav from './Components/Nav/Nav'
 
 function App() {
 
   return (
-    <>
-      <Nav/> 
+    <> 
+      <Nav/>
+      <Header/>
     </>
   )
 }
