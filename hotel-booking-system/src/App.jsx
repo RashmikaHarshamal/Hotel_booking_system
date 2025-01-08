b56af2d3b8af9cbfdb2b6bf3b0fe4d0fa1131d90
Rashmika_Harshamal <rashmikaharshamal169@mail.com>
2025-01-08 23:26:52 +0530

    Added Header component with Swiper integration and updated styles

--- hotel-booking-system/src/App.jsx
+++ hotel-booking-system/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Nav from './Components/Nav/Nav'
+import About from './Components/About/About'
 
 function App() {
 
--- hotel-booking-system/src/Components/About/About.jsx
+++ hotel-booking-system/src/Components/About/About.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import headerCSS from './../About/About.module.css';
+
+import aboutImg from './../../assets/about-one.png';
 
 function About() {
     return(
-        <div className="about">
-            <h1>About</h1>
+        <div className={`${aboutCSS.about_wrapper} section`}>
+            <div className={aboutCSS.about_img}>
+                <img src={aboutImg} alt="About the Royal Hotel" />
+            </div>
+            <div className={aboutCSS.about_content}>
+                <small>The Royal Hotel</small>
+                <h2 className="section_Title">Where Elegance Meets <span>Excellence</span></h2>
+
+            </div>
         </div>
     )
 }
